81bcb0ab8c98814a0879901af3eff1d92abfb602
satiu123 <s13658395102@gmail.com>
2026-09-10 20:31:09 +0800

    chore: bump version to 2.0.25200.2

--- Celestite/Build.cs
+++ Celestite/Build.cs
@@ -3,6 +3,6 @@ namespace Celestite;
 public class Build
 {
     public const string Channel = "Beta";
-    public const string Version = "2.0.25200.1";
+    public const string Version = "2.0.25200.2";
     public const string BuildId = $"{Channel} {Version}";
 }
